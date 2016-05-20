0c04a50d5746f2237f977efbad2a1ec8d4b0bdac
bdargham <basheer@dargham.net>
2016-05-20 17:57:44 +0200

    Update Stepper Driver.ino

--- Stepper Driver.ino
+++ Stepper Driver.ino
@@ -1,146 +1,52 @@
-// define the stepper driver pins
 #define IN1  8
 #define IN2  9
 #define IN3  10
 #define IN4  11
+#define STEPS_PER_ROTATION 4096
 
-// define how many cycles to a full rotation
-#define CYCLES_PER_ROTATION 512
-
-void setup() 
+void setup()
 {
-  Serial.begin(9600);
-  pinMode(IN1, OUTPUT); 
-  pinMode(IN2, OUTPUT); 
-  pinMode(IN3, OUTPUT); 
-  pinMode(IN4, OUTPUT); 
-
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  delay(100);
 }
 
-void loop() 
+void loop()
 {
-  // make the stepper do 1.5 rotations CW
-  turns(1.5);
-  delay(1000);
-  // make the stepper do 1.5 rotations CCW
-  turns(-1.5);
-  delay(1000);
-
+  rotate(1.5);
+  delay(2000);
+  rotate(-1.5);
+  delay(2000);
 }
 
-void turns(float rotations)
+void rotate(float rotations) {
+  rotate_steps(rotations * STEPS_PER_ROTATION);
+}
+
+int phase = 0;
+byte phases[] = { 1, 3, 2, 6, 4, 12, 8, 9 };
+long lastutime;
+void rotate_steps(int steps)
 {
-  // if the rotation count is -ve then it is CCW
-  Serial.println();
-  Serial.print("Turning : ");
-  Serial.print(rotations);
-  Serial.println(" rotations");
-  bool clockwise = rotations > 0;
-  Serial.print("Clockwise = ");
-  Serial.println(clockwise);
-  // calculate how many cycles the stepper will have to make
-  int cycles = rotations * CYCLES_PER_ROTATION;
-  // force the cycle count to be positive
-  cycles = abs(cycles);
-  Serial.print("That is ");
-  Serial.print(cycles);
-  Serial.print(" Cycles ");
-  // only move if the user specifed an actual movement
-  if(rotations != 0)
-  {
-    if (clockwise)
-    {
-      Serial.println("Clockwise");
-      // for each cycle
-      for (int x=0; x<cycles; x++)
-      {
-        // for each phase
-        for(int y=0; y<8; y++)
-        {
-          // go to phase y
-          phaseSelect(y);
-          // pause so the stepper has time to react
-          delay(1);
-        }
-      }
-    } else {
-      Serial.println("Counter Clockwise");
-      // for each cycle
-      for (int x=0; x<cycles; x++)
-      {
-        // for each phase (backwards for CCW rotation)
-        for(int y=7; y>=0; y--)
-        {
-          // go to phase y
-          phaseSelect(y);
-          // pause so the stepper has time to react
-          delay(1);
-        }
-      }
-    }
+  int dir = (steps > 0) - (steps < 0);
+  steps *= dir;
+  long laststep;
+  for (int i = 0; i < steps;) {
+    long now = micros();
+    if (now - laststep < 1000) continue;
+    laststep = now;
+    stepper_writepins(phases[phase]);
+    phase = (8 + phase + dir) % 8;
+    i++;
   }
-  // go to the default state (all poles off) when finished
-  phaseSelect(8);
-  Serial.println("Done");
+  stepper_writepins(0);
 }
 
-void phaseSelect(int phase)
-{
-  switch(phase){
-     case 0:
-       digitalWrite(IN1, LOW); 
-       digitalWrite(IN2, LOW);
-       digitalWrite(IN3, LOW);
-       digitalWrite(IN4, HIGH);
-       break; 
-     case 1:
-       digitalWrite(IN1, LOW); 
-       digitalWrite(IN2, LOW);
-       digitalWrite(IN3, HIGH);
-       digitalWrite(IN4, HIGH);
-       break; 
-     case 2:
-       digitalWrite(IN1, LOW); 
-       digitalWrite(IN2, LOW);
-       digitalWrite(IN3, HIGH);
-       digitalWrite(IN4, LOW);
-       break; 
-     case 3:
-       digitalWrite(IN1, LOW); 
-       digitalWrite(IN2, HIGH);
-       digitalWrite(IN3, HIGH);
-       digitalWrite(IN4, LOW);
-       break; 
-     case 4:
-       digitalWrite(IN1, LOW); 
-       digitalWrite(IN2, HIGH);
-       digitalWrite(IN3, LOW);
-       digitalWrite(IN4, LOW);
-       break; 
-     case 5:
-       digitalWrite(IN1, HIGH); 
-       digitalWrite(IN2, HIGH);
-       digitalWrite(IN3, LOW);
-       digitalWrite(IN4, LOW);
-       break; 
-     case 6:
-       digitalWrite(IN1, HIGH); 
-       digitalWrite(IN2, LOW);
-       digitalWrite(IN3, LOW);
-       digitalWrite(IN4, LOW);
-       break; 
-     case 7:
-       digitalWrite(IN1, HIGH); 
-       digitalWrite(IN2, LOW);
-       digitalWrite(IN3, LOW);
-       digitalWrite(IN4, HIGH);
-       break; 
-     default:
-       digitalWrite(IN1, LOW); 
-       digitalWrite(IN2, LOW);
-       digitalWrite(IN3, LOW);
-       digitalWrite(IN4, LOW);
-       break; 
-  }
+void stepper_writepins(int bitmap) {
+    digitalWrite(IN1, bitmap & 8 ? HIGH : LOW);
+    digitalWrite(IN2, bitmap & 4 ? HIGH : LOW);
+    digitalWrite(IN3, bitmap & 2 ? HIGH : LOW);
+    digitalWrite(IN4, bitmap & 1 ? HIGH : LOW);
 }
-
